abba3a12904f3c80aaa25536bf5fdf83b036c87f
threexc <tvgamblin@gmail.com>
2018-07-20 15:25:48 -0400

    Added unfinished module management scripts

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SiGPyC',
+    version='0.1dev',
+    packages=['sigpyc',]
+    license='MIT License'
+    long_description=open('README.md').read()
+)
