4d66b3150a79b8634affb64593059318908a2a78
threexc <tvgamblin@gmail.com>
2018-07-26 17:29:43 -0400

    Removed setup.py - it is unnecessary in the current project format

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='SiGPyC',
-    version='0.1dev',
-    packages=['sigpyc',]
-    license='MIT License'
-    long_description=open('README.md').read()
-)
