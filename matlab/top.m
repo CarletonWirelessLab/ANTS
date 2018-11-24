31be709cf24f159cbea944beac3012c06234efdf
shadyelkamhawy <shadyelkamhawy@cmail.carleton.ca>
2018-11-24 15:36:01 -0500

    matlab cleanup, top file, organized figure numbers, removed unneeded variables

--- matlab/top.m
+++ matlab/top.m
@@ -1,7 +1,24 @@
+%% Top file that calls the other functions (find_IFSOnly + Load_and_Eval_IFSOnly)
+%% should be converted to python script that accepts arguments
+%% arguments:
+% filename
+% accessCategory
+% mode
 
-mode = 'Supervisey';
 
+fileName = '0.bin';
+accessCategory = 0; % 0 = Voice, 1 = Video, 2 = Best Effort, 3 = BackGround
+IntDuration = 0; % mSec
+sampRate = 20e6;
+duration = inf;       % In seconds
+packetIndex = 3;
+f = 0.02;
+% Find packetstart and end epochs
+[locs, threshold] = detectPacketLocations(fileName, sampRate, duration,f); %Kareem's code
+IFS = (locs(2:end, 1) - locs(1:(end-1), 2))/sampRate * 1e6;
 backOff = IFS;
+%% Save Output
+save([fileName(1:end-4) '.mat'],'IFS','locs','sampRate', 'accessCategory', 'IntDuration');
 
 figName = 'xx';
 switch accessCategory
@@ -43,16 +60,13 @@ histScales(2, backOff, 100, gtitle, xlab, 1, [0 min(ceil(max(backOff)), 250)]);
 saveas(gcf,['b_' figName '.jpg'])
 
 %%%% Compliance Calculator 
-[Bins, prob , probMax, TxopDurations] = EN_301_893_ReqCalc(IFS, locs/sampRate * 1e6, accessCategory, mode);
+[Bins, prob , probMax, TxopDurations] = EN_301_893_ReqCalc(IFS, locs/sampRate * 1e6, accessCategory);
 
 meanTxop = mean(TxopDurations)
 maxTxop = max(TxopDurations)
 
-[SIFSFactor, TxopFactor, AGFactor, KBL, NF] = ComplianceMetric_v101(IFS, SIFSD, locs/sampRate * 1e6, accessCategory, mode);
+[SIFSFactor, TxopFactor, AGFactor, KBL, NF] = ComplianceMetric_v101(IFS, SIFSD, locs/sampRate * 1e6, accessCategory);
 name = 'GOOGLE';
 
-%dlmwrite('output.csv',name,'-append')
 output = {accessCategory, length(IFS), maxTxop, sum(prob > probMax), TxopFactor, AGFactor, KBL, 1 - exp(KBL), SIFSFactor, NF};
 dlmwrite('output.csv',output,'-append')
-
-
